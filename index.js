5ce0ddddc7b10c203903974f6c59828a11ef2971
Sourin Roy <144391871+sourinry@users.noreply.github.com>
2026-03-23 15:50:05 +0530

    import cors & add cors setting in index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import "dotenv/config";
-
+import cors from 'cors';
 import { connectDB } from "./config/db.js";
 import express from "express";
 const app = express();
 
+//import index-route
 import router from './routes/indexRoute.js';
+
+// Configure CORS with specific options
+const corsOptions = {
+  // Allow only these origins
+  origin: [
+    'https://localhost:3000',
+    'https://localhost:4200',
+    'https://localhost:8080',
+  ],
+  // Allow these HTTP methods
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+};
 
 //middleware
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use('/api/v1/', router);
 
-// ERROR HANDLER
+// error handaler
 import { errorHandler } from "./middlewares/errorMiddleware.js";
 app.use(errorHandler);
 
@@ -29,4 +43,5 @@ const startServer = async () => {
   }
 };
 
+//call the startServer function
 startServer();
